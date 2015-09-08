2e2f916d3ad16485d50af3b52e29c6a20e381ad6
Phi Lieu <phihl79@gmail.com>
2015-09-08 14:25:09 +1000

    Add more tests

--- EntityFramework.Repository.Tests/BasicOperationTests.cs
+++ EntityFramework.Repository.Tests/BasicOperationTests.cs
@@ -1,30 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Autofac;
 using EntityFramework.Repository.Tests.Data;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace EntityFramework.Repository.Tests
 {
     [TestFixture]
-    public class BasicOperationTests
+    public class BasicOperationTests : TestBase 
     {
-        [Test]
-        public void Test1()
+        private Transaction _transaction;
+        private Account _account;
+
+        [SetUp]
+        public void SetUp()
         {
+            var accountName = DateTime.Now.ToString("yyyyMMddTHH:mm:ss.fff");
+            var transactionDescription = DateTime.Now.ToString("yyyyMMddTHH:mm:ss.fff");
+            _account = new Account
+            {
+                Name = accountName,
+                Balance = 10,
+            };
+            _transaction = new Transaction
+            {
+                Description = transactionDescription,
+                Amount = 20
+            };
+            _account.Transactions.Add(_transaction);
+
+        }
+
+        [Test]
+        public void Should_work_with_an_ioc_container()
+        {
+            // Arrange
             var accountRepository = GlobalSetup.Container.Resolve<IRepository<ITestDbContext, Account>>();
             var unitOfWork = GlobalSetup.Container.Resolve<IUnitOfWork<ITestDbContext>>();
-            accountRepository.Insert(new Account
-            {
-                Name = DateTime.Now.ToString(CultureInfo.CurrentCulture),
-                Balance = 10,
-            });
 
+            // Act
+            accountRepository.Insert(_account);
             unitOfWork.SaveChanges();
+
+            // Assert
+            HasAccount(_account .Name).Should().BeTrue();
+            HasTransaction(_account.Name, _transaction.Description).Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_work_without_an_ioc_container()
+        {
+            // Arrange
+            // Act
+            using (var context = new TestDbEntities())
+            using (var contextAdapter = new ContextAdaptor<ITestDbContext>(context))
+            using (var unitOfWork = new UnitOfWork<ITestDbContext>(contextAdapter))
+            {
+                var accountRepository = new Repository<ITestDbContext, Account>(contextAdapter);
+                accountRepository.Insert(_account);
+
+                unitOfWork.SaveChanges();
+            }
+
+            // Assert
+            HasAccount(_account.Name).Should().BeTrue();
+            HasTransaction(_account.Name, _transaction.Description).Should().BeTrue();
         }
     }
 }
---
+++ EntityFramework.Repository.Tests/TestBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework.Repository.Tests.Data;
+using NUnit.Framework;
+
+namespace EntityFramework.Repository.Tests
+{
+    public abstract class TestBase
+    {
+        protected bool HasAccount(string accountName)
+        {
+            using (var context = new TestDbEntities())
+            {
+                return context.Accounts.Any(acc => acc.Name == accountName);
+            }
+        }
+
+        protected bool HasTransaction(string accountName, string transactionDescription)
+        {
+            using (var context = new TestDbEntities())
+            {
+                return
+                    context.Accounts.Any(
+                        acc =>
+                            acc.Name == accountName &&
+                            acc.Transactions.Any(tr => tr.Description == transactionDescription));
+            }
+        }
+
+        [SetUp]
+        public void SetUpBase()
+        {
+            using (var context = new TestDbEntities())
+            {
+                context.Transactions.RemoveRange(context.Transactions.ToList());
+                context.Accounts.RemoveRange(context.Accounts.ToList());
+                context.SaveChanges();
+            }
+        }
+    }
+}
